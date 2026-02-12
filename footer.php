c76f4ad8fc0be12e962f39c86ce4726c166b8bd3
unknown <surekhaghugal1-create>
2026-02-12 15:51:46 +0530

    change code email section

--- footer.php
+++ footer.php
@@ -59,7 +59,7 @@
     <title>Quadra Horizon</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 
